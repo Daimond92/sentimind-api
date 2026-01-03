003aec3b09a18a1f6512b51d0b8addc21e1835ef
Natalia <nattypavez@gmail.com>
2026-01-03 12:17:02 -0300

    Cleanup project structure and PostgreSQL persistence setup

--- src/main/java/com/sentimind/sentimind_api/repository/SentimentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sentimind.sentimind_api.repository;
-
-import com.sentimind.sentimind_api.model.SentimentAnalysis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SentimentRepository extends JpaRepository <SentimentAnalysis, Long> {
-}
